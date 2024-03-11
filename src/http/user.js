fad4d669f99f5ce307b61d8227cc9ec511d2816b
zhangxinxin <1285367184@qq.com>
2024-03-11 23:41:24 +0800

    ✨feat: 接口联调完成

--- src/http/user.js
+++ src/http/user.js
@@ -1,33 +1,66 @@
 import http from './index'
 
+// 登录
 export const login = (data) => {
   return http.post('/user/login', data)
 }
 
+// 查询心情
 export const listMood = (data) => {
   return http.post('/mood-records/list', data)
 }
 
+// 添加心情
 export const addMood = (data) => {
   return http.post('/mood-records/add', data)
 }
 
+// 查询自我探知
 export const listSelf = (data) => {
   return http.post('/self-discovery/list', data)
 }
 
+// 新增自我探知
 export const addSelf = (data) => {
   return http.post('/self-discovery/add', data)
 }
 
+// 查新用户信息
 export const listUserInfo = (data) => {
   return http.post('/user/list', data)
 }
 
+// 更新用户信息
 export const updateUser = (data) => {
   return http.post('/user/update', data)
 }
 
+// 注册用户
 export const register = (data) => {
   return http.post('/user/register', data)
+}
+
+// 查询所有用户列表
+export const userAll = () => {
+  return http.post('/user/list', { pageNumber: 1, pageSize: 10000 })
+}
+
+// 发送信件
+export const sendLetter = (data) => {
+  return http.post('/correspondence/add', data)
+}
+
+// 根据当前用户id查询已发送的所有信件
+export const listsendLetterAllByUserId = (data) => {
+  return http.post('/correspondence/list', data)
+}
+
+// 根据当前用户id查询收件
+export const listAddresseeListByUserId = (data) => {
+  return http.post('/correspondence/list', data)
+}
+
+// 拒收信件
+export const deleteLetter = (data) => {
+  return http.post('/correspondence/delete', data)
 }
--- src/views/Header.vue
+++ src/views/Header.vue
@@ -51,36 +51,64 @@
     v-model:open="open"
     title="信件"
     @ok="handleOk"
-    style="width: 800px"
+    @cancel="handleCancel"
+    style="width: 1200px"
     cancelText="关闭"
     okText="发送"
   >
     <a-tabs v-model:activeKey="activeKey">
-      <a-tab-pane key="1" tab="自我探知问卷">
+      <a-tab-pane key="1" tab="发送信件">
         <a-card :bordered="false">
-          <a-form :label-col="{ style: { width: '80px' } }">
-            <a-form-item label="收件人">
-              <a-select
-                ref="select"
-                v-model:value="value1"
-                style="width: 120px"
-                @focus="focus"
-                @change="handleChange"
-              >
-                <a-select-option value="jack">Jack</a-select-option>
-                <a-select-option value="lucy">Lucy</a-select-option>
-                <a-select-option value="Yiminghe">yiminghe</a-select-option>
+          <a-form ref="letterFormRef" :model="letterForm" :label-col="{ style: { width: '80px' } }">
+            <a-form-item
+              label="收件人"
+              name="addresseeId"
+              :rules="[{ required: true, message: '请选择收件人!' }]"
+            >
+              <a-select v-model:value="letterForm.addresseeId" ref="select" style="width: 120px">
+                <template v-for="item in userList" :key="item">
+                  <a-select-option :value="item.id">{{ item.username }}</a-select-option>
+                </template>
               </a-select>
             </a-form-item>
 
-            <a-form-item label="内容">
-              <a-textarea></a-textarea>
+            <a-form-item
+              label="内容"
+              name="content"
+              :rules="[{ required: true, message: '请输入信件内容!' }]"
+            >
+              <a-textarea v-model:value="letterForm.content"></a-textarea>
             </a-form-item>
           </a-form>
         </a-card>
       </a-tab-pane>
-      <a-tab-pane key="3" tab="历史记录">
-        <a-table :dataSource="dataSource" :columns="columns" style="width: 100%" />
+
+      <a-tab-pane key="2" tab="收件箱">
+        <a-table
+          :pagination="addresseePagination"
+          @change="handleAddresseePageParams"
+          :dataSource="addresseeDataSource"
+          :columns="addresseeColumns"
+          style="width: 100%"
+        >
+          <template #bodyCell="{ column, text, record }">
+            <template v-if="column.dataIndex === 'operation'">
+              <a-popconfirm title="您确定要拒收吗?" @confirm="onDelete(record.id)">
+                <a-button type="primary">拒收信件</a-button>
+              </a-popconfirm>
+            </template>
+          </template>
+        </a-table>
+      </a-tab-pane>
+
+      <a-tab-pane key="3" tab="已发送信件">
+        <a-table
+          :pagination="pagination"
+          @change="handlePageParams"
+          :dataSource="letterDataSource"
+          :columns="columns"
+          style="width: 100%"
+        />
       </a-tab-pane>
     </a-tabs>
   </a-modal>
@@ -88,12 +116,33 @@
 
 <script setup>
 import { useRouter, useRoute } from 'vue-router'
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import { deleteLetter, listsendLetterAllByUserId, sendLetter, userAll } from '@/http/user'
+import { message } from 'ant-design-vue'
+
+onMounted(async () => {
+  const res = await userAll()
+  userList.value = res.data.body.dataList
+  listAddresseeData()
+  listSendLetterData()
+})
+
 // 1
 const router = useRouter()
 const route = useRoute()
-const value1 = ref('lucy')
 const open = ref(false)
+const activeKey = ref('1')
+const pagination = ref({
+  pageSize: 10,
+  pageNumber: 1,
+  total: 0
+})
+
+const addresseePagination = ref({
+  pageSize: 10,
+  pageNumber: 1,
+  total: 0
+})
 
 const handleMenu = (index) => {
   switch (index) {
@@ -115,45 +164,168 @@ const handleMenu = (index) => {
   }
 }
 
-const handleLetter = () => {
+const userList = ref([]) // 用户列表
+const letterForm = ref({}) // 信件表单
+const letterFormRef = ref() // 信件表单ref
+
+const handleLetter = async () => {
   open.value = true
 }
 
-const handleOk = (e) => {
+const handleCancel = () => {
+  letterForm.value = {}
   open.value = false
+  activeKey.value = '1'
+}
+
+const handleOk = async () => {
+  letterFormRef.value
+    .validate()
+    .then(async () => {
+      letterForm.value.senderId = Number(localStorage.getItem('userId'))
+
+      const res = await sendLetter(letterForm.value)
+      open.value = false
+      message.success(res.data.reMsg)
+      listSendLetterData()
+    })
+    .catch((error) => {
+      message.success(error.data.reMsg)
+    })
 }
 const columns = ref([
   {
-    title: '收件人',
-    dataIndex: 'person',
-    key: 'person'
+    title: '发件人',
+    dataIndex: 'senderId',
+    key: 'senderId',
+    align: 'center',
+    customRender: ({ record }) => {
+      return localStorage.getItem('username')
+    }
   },
   {
+    title: '收件人',
+    dataIndex: 'addresseeId',
+    key: 'addresseeId',
+    align: 'center',
+    customRender: ({ record }) => {
+      return userList.value.map((item) => {
+        if (item.id == record.addresseeId) {
+          return item.username
+        }
+      })
+    }
+  },
+  {
+    title: '审核状态',
+    dataIndex: 'audit',
+    key: 'audit',
+    align: 'center',
+    customRender: ({ record }) => {
+      return record.audit == 1
+        ? '审核通过'
+        : record.audit == 2
+          ? '待审核'
+          : record.audit === 0
+            ? '未通过'
+            : ''
+    }
+  },
+
+  {
     title: '内容',
-    dataIndex: 'desc',
-    key: 'desc',
+    dataIndex: 'content',
+    key: 'content',
+    width: '60%'
+  }
+])
+
+const addresseeColumns = ref([
+  {
+    title: '发件人',
+    dataIndex: 'senderId',
+    key: 'senderId',
+    align: 'center',
+    customRender: ({ record }) => {
+      return localStorage.getItem('username')
+    }
+  },
+  {
+    title: '收件人',
+    dataIndex: 'addresseeId',
+    key: 'addresseeId',
+    align: 'center',
+    customRender: ({ record }) => {
+      return userList.value.map((item) => {
+        if (item.id == record.addresseeId) {
+          return item.username
+        }
+      })
+    }
+  },
+  {
+    title: '审核状态',
+    dataIndex: 'audit',
+    key: 'audit',
+    align: 'center',
+    customRender: ({ record }) => {
+      return record.audit == 1
+        ? '审核通过'
+        : record.audit == 2
+          ? '待审核'
+          : record.audit === 0
+            ? '未通过'
+            : ''
+    }
+  },
+
+  {
+    title: '内容',
+    dataIndex: 'content',
+    key: 'content',
     width: '60%'
   },
   {
-    title: '发送时间',
-    dataIndex: 'time',
-    key: 'time'
+    title: '操作',
+    dataIndex: 'operation'
   }
 ])
-const dataSource = ref([
-  {
-    key: '1',
-    person: '周杰伦',
-    desc: '测试内容1',
-    time: '2024-03-05 08:30'
-  },
-  {
-    key: '2',
-    person: '刘德华',
-    desc: '测试内容2',
-    time: '2024-03-04 07:30'
-  }
-])
+const letterDataSource = ref([])
+const addresseeDataSource = ref([])
+
+const handlePageParams = (page, pageSize) => {
+  pagination.value.pageNumber = page.current
+  listSendLetterData()
+}
+
+const handleAddresseePageParams = (page, pageSize) => {
+  pagination.value.pageNumber = page.current
+  listSendLetterData()
+}
+
+const listSendLetterData = async () => {
+  const userId = Number(localStorage.getItem('userId'))
+  const { data } = await listsendLetterAllByUserId({ ...pagination.value, senderId: userId })
+  letterDataSource.value = data.body.dataList
+  pagination.value.total = data.body.allTotal
+}
+
+const listAddresseeData = async () => {
+  const userId = Number(localStorage.getItem('userId'))
+  const { data } = await listsendLetterAllByUserId({
+    ...addresseePagination.value,
+    addresseeId: userId
+  })
+  addresseeDataSource.value = data.body.dataList
+  addresseePagination.value.total = data.body.allTotal
+}
+
+const onDelete = async (letterId) => {
+  const res = await deleteLetter({ id: letterId })
+  console.log(res)
+  message.success(res.data.reMsg)
+  listAddresseeData()
+}
 </script>
 
 <style lang="scss" scoped>
